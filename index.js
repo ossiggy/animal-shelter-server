b2fd2f78dafdbf33a7eba73fb4f186364ca87510
Ossiggy <mrossig201@gmail.com>
2017-11-20 21:22:36 -0500

    get and delete endpoints working

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const {cats, dogs} = require('./data')
 
 const {PORT, CLIENT_ORIGIN} = require('./config');
 const {dbConnect} = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
+
 
 const app = express();
 
@@ -19,6 +21,26 @@ app.use(
         origin: CLIENT_ORIGIN
     })
 );
+
+app.get('/api/cat', (req, res) => {
+  res.json(cats[0]);
+})
+
+app.get('/api/dog', (req, res) => {
+  res.json(dogs[0]);
+})
+
+app.delete('/api/cat', (req,res) => {
+  cats.shift();
+  res.sendStatus(200);
+  console.log(cats);
+})
+
+app.delete('/api/dog', (req,res) => {
+  dogs.shift();
+  res.sendStatus(200);
+  console.log(dogs)
+})
 
 function runServer(port = PORT) {
     const server = app
